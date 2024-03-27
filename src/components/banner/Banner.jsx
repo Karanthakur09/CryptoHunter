22b6f3ce4f14d57850f31de0790f327a3d9f1665
Karan <kstkaranthakur03@gmail.com>
2024-03-28 02:46:08 +0530

    adding carousel

---
+++ src/components/banner/Banner.jsx
@@ -0,0 +1,53 @@
+import { Container, Typography } from '@mui/material'
+import React from 'react'
+import Carousel from './Carousel'
+
+function Banner() {
+    return (
+        <div style={{ backgroundImage: "url(./banner2.jpg)" }}>
+            <Container sx={{
+                height: 400,
+                display: "flex",
+                flexDirection: "column",
+                paddingTop: 6,
+                justifyContent: "space-around",
+            }}>
+                <div style={{
+                    display: "flex",
+                    height: "40%",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    textAlign: "center",
+                }}>
+                    <Typography
+                        variant='h2'
+                        sx={{
+                            fontWeight: "bold",
+                            marginBottom: 2,
+                            fontFamily: "Montserrat",
+                        }}
+                    >
+                        Crypto Hunter
+                    </Typography>
+                    <Typography 
+                    variant='subtitle2'
+                    sx={{
+                        marginBottom: 6,
+                        color: "darkgrey",
+                        textTransform: "capitalize",
+                        fontFamily: "Montserrat",
+                    }}>
+                        Get all the Info regarding your favorite Crypto Currency
+
+                    </Typography>
+                    <Carousel/>
+
+                </div>
+
+            </Container>
+
+        </div>
+    )
+}
+
+export default Banner
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import Banner from '../components/banner/Banner'
 
 function HomePage() {
   return (
-    <div>HomePage</div>
+    <Banner />
   )
 }
 
